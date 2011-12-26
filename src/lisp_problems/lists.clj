e1049e40a93227649b42f54e00e3f3b8a00c1a65
Owain Lewis <owainlewis@Owains-MacBook-Pro.local>
2011-12-26 13:01:12 +0000

    Solved first two problems

---
+++ src/lisp_problems/lists.clj
@@ -0,0 +1,39 @@
+(ns lisp-problems.lists)
+
+; P01 (*) Find the last box of a list.
+;    Example:
+;    * (my-last '(a b c d))
+;    (D)
+
+; Easy one line solution
+(defn my-last [x]
+  (last x))
+
+(defn my-last-2 [x]
+  (reduce #(do %2) x))
+
+; I like this one
+(defn my-last-3 [x]
+  (if (empty? (rest x))
+    (first x)
+    (my-last-3 (rest x))))
+
+;P02 (*) Find the last but one box of a list.
+;    Example:
+;    * (my-but-last '(a b c d))
+;    (C D)
+
+(defn my-but-last [x]
+  (first (rest (reverse x))))
+
+(defn my-but-last-2 [x]
+  (if (empty? (rest (rest x)))
+    (first x)
+    (my-but-last-2 (rest x))))
+
+; Get the length of the list and find the item at len - 1
+(defn my-but-last-3 [x]
+  (let [len (count x)]
+    (nth x (dec (dec len)))))
+
+ 
--- test/lisp_problems/test/lists.clj
+++ test/lisp_problems/test/lists.clj
@@ -1,4 +1,19 @@
-(ns lisp-problems.test.lists
+(ns lisp_problems.test.lists
   (:use [lisp-problems.lists])
   (:use [clojure.test]))
+
+(deftest test-returns-last-element-of-list
+  "Should return the last element of a list"
+  (let [x (list 1 2 3 4 5)]
+    (is (= (my-last x) 5))
+    (is (= (my-last-2 x) 5))
+    (is (= (my-last-3 x) 5))))
+
+(deftest test-returns-last-but-one-of-list
+  "Should return the last but one element of a list"
+  (let [x (list 1 2 3 4 5)]
+    (is (= (my-but-last x) 4))
+    (is (= (my-but-last-2 x) 4))
+    (is (= (my-but-last-3 x) 4))))
+
 
